b38e5cbf6bffcd523cc4c75d688681567ca8526b
Gustavo Consoleti <51282187+Gustavo-CRS@users.noreply.github.com>
2021-11-04 12:36:53 -0300

    Criação do shared com as cores e mudanças nos temas

---
+++ lib/app/shared/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Color(0xFF111111);
+  static const purpleButton = Color(0xFF5520C5);
+  static const titleColor = Color(0xFF453C93);
+}
